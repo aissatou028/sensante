8201d8cd7c9c7f08844b0c1a7259005ac079c35e
Mohamed Mourtada KAMARA <mouhakamara67@gmail.com>
2026-05-01 23:02:55 +0000

    Fix import prisma singleton (Pilote)

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,13 +1,16 @@
 import { PrismaClient } from "@prisma/client";
-import { PrismaPg } from "@prisma/adapter-pg";
 
-const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClient | undefined;
+const prismaClientSingleton = () => {
+  return new PrismaClient();
 };
 
-const adapter = new PrismaPg({ connectionString: "postgresql://postgres:azerty@localhost:5432/sensante?schema=public" });
+declare global {
+  // eslint-disable-next-line no-var
+  var prismaGlobal: undefined | ReturnType<typeof prismaClientSingleton>;
+}
 
-export const prisma = globalForPrisma.prisma ?? new PrismaClient({ adapter });
+const prisma = globalThis.prismaGlobal ?? prismaClientSingleton();
 
-if (process.env.NODE_ENV !== "production")
-  globalForPrisma.prisma = prisma;
+export default prisma;
+
+if (process.env.NODE_ENV !== "production") globalThis.prismaGlobal = prisma;
